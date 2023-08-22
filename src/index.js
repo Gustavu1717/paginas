7b3c7e3ab2fa782bd3e231830ed7f11472a887df
Kauan <mvkauan4@gmail.com>
2023-08-22 17:21:10 -0300

    o

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.scss';
 import Login from './pages/login';
-
+import Cadastro from './pages/cadastro';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +11,8 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login/>} />
+        <Route path='/cadastro' element={<Cadastro/>} />
+
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
